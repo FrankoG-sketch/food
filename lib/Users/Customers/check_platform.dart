4942428affba1b406accdabab18c0bcc1ffaa817
mc100p <Campbellmikhail26@gmail.com>
2021-12-23 00:16:28 -0500

    changes made to make login and sign up possible

--- lib/Users/Customers/check_platform.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class CheckPlatform {
-  final BuildContext context;
-  const CheckPlatform({this.context});
-  isIOS() {
-    return Theme.of(context).platform == TargetPlatform.iOS ? true : false;
-  }
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,62 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+// import 'package:get/get.dart';
+import 'package:shop_app/pages/homePage.dart';
+import 'package:shop_app/pages/signIn.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 //import 'package:shop_app/Users/Supermarket/home.dart';
-import 'package:shop_app/routes.dart';
+import 'package:shop_app/utils/routes.dart';
 //import 'package:shop_app/screens/home/home_screen.dart';
 //import 'package:shop_app/screens/profile/profile_screen.dart';
-import 'package:shop_app/Authentication/splash/splash_screen.dart';
+// import 'package:shop_app/Authentication/splash/splash_screen.dart';
 //import 'package:firebase_core/firebase_core.dart';
 
-import 'package:shop_app/theme.dart';
+// import 'constants/firebase.dart';
+// import 'controllers/appController.dart';
+// import 'controllers/authController.dart';
+// import 'controllers/cart_controller.dart';
+// import 'controllers/products_controller.dart';
 
-import 'constants/firebase.dart';
-import 'controllers/appController.dart';
-import 'controllers/authController.dart';
-import 'controllers/cart_controller.dart';
-import 'controllers/products_controller.dart';
-
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await initialization.then((value) {
-    Get.put(AppController());
-    Get.put(UserController());
-    Get.put(ProducsController());
-    Get.put(CartController());
-  });
+  final title = "Food";
+  await Firebase.initializeApp();
+  // await initialization.then((value) {
+  //   Get.put(AppController());
+  //   Get.put(UserController());
+  //   Get.put(ProducsController());
+  //   Get.put(CartController());
+  //});
+  SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+  String login = sharedPreferences.getString('email');
+  print("Login: " + login.toString());
 
-  runApp(MyApp());
+  runApp(MyApp(title: title, login: login));
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({
+    Key key,
+    this.title,
+    this.login,
+  }) : super(key: key);
+
+  final String title;
+  final String login;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: theme(),
+
       //home: HomeScreen(),
-      home: SplashScreen(), //Cals(),
+      home: login != null ? HomePage() : SignIn(),
+
+      //Cals(),
       // We use routeName so that we dont need to remember the name
-      initialRoute: SplashScreen.routeName,
-      routes: routes,
+      initialRoute: "/",
+      onGenerateRoute: RouteGenerator.generateRoute,
     );
   }
 }
